00cbd8de11e9c6c1d4169512d52cc99d9801c5e2
sami-ahmad-1 <133954576+sami-ahmad-1@users.noreply.github.com>
2024-01-12 11:31:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,26 +26,6 @@ function ageCalculate() {
 
     let d3, m3, y3;
 
-    // y3 = y2 - y1;
-    // if (m2 >= m1) {
-    //     m3 = m2 - m1;
-    // } else {
-    //     y3--;
-    //     m3 = 12 + m2 - m1;
-    // } 
-
-    // if (d2 >= d1) {
-    //     d3 = d2 - d1;
-    // } else {
-    //     m3--;
-    //     d3 = getDaysInMonth(y1, m1) + d2 - d1;
-    // }
-
-    // if (m3 < 0) {
-    //     m3 = 11;
-    //     y3--;
-    // }
-
     y3 = y2 - y1;
     m3 = (m2 >= m1) ? m2 - m1 : (y3--, 12 + m2 - m1);
     d3 = (d2 >= d1) ? d2 - d1 : (m3--, getDaysInMonth(y1, m1) + d2 - d1);
